99c939dff656ca270ca5ef1d4d0cb7482f9c3c37
KZS <KZS@a.a>
2024-04-23 08:39:23 +0200

    oraimunka

---
+++ gallery.js
@@ -0,0 +1,8 @@
+console.log('HELLO WORLD')
+
+let container = document.querySelector('#container')
+
+let ujDiv = document.createElement('div')
+console.log(ujDiv)
+ujDiv.innerText = 'HELLO WORLD'
+container.appendChild(ujDiv)
